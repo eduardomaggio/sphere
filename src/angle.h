132c89da1eeece505b55e27a93b6f2b864c9a726
MAGGIO <eduardomaggio@github.com>
2015-08-23 10:11:43 -0400

    Implement angle magnitude conversion

---
+++ src/angle.h
@@ -0,0 +1,12 @@
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+
+// converts angle magnitude from Degree to Radians
+#define DEG2RAD(A) (A*M_PI/180.0)
+
+
+// converts angle magnitude from Radians to Degrees
+#define RAD2DEG(A) (A*180.0*M_1_PI)
+
+
