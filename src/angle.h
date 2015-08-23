f1c0af6d482a371af18c02ebca3276d4c3f5b410
eduardomaggio <eduardomaggio@users.noreply.github.com>
2015-08-23 12:07:40 -0300

    Implementation of algle normalization funcions

--- src/angle.h
+++ src/angle.h
@@ -22,11 +22,77 @@
 
 #include <math.h>
 
+namespace maggio
+{
+
 // converts angle magnitude from Degree to Radians
 #define DEG2RAD(A) (A*M_PI/180.0)
-
 
 // converts angle magnitude from Radians to Degrees
 #define RAD2DEG(A) (A*180.0*M_1_PI)
 
+// some definitions 
 
+#define DEGREE_FULL_ARC (360.0)
+#define DEGREE_HALF_ARC (180.0)
+
+#define RADIAN_FULL_ARC (M_PI*2.0)
+#define RADIAN_HALF_ARC (M_PI)
+
+
+// general wrap
+inline double NormalizeAngle( 
+  double angle, 
+  const double offset,
+  const double full_arc,
+  const double half_arc)
+  {
+    double resp = fmod(angle,full_arc) + offset;
+    if ( resp < 0 )
+      resp += full_arc;
+    return resp - offset;
+  }
+
+
+// wrap angle (deg) to the interval [0, 360.0)
+inline double UnsignedAngleDeg( double angle )
+{
+  return NormalizeAngle(
+    angle,
+    0,
+    DEGREE_FULL_ARC,
+    DEGREE_HALF_ARC);
+}
+
+
+// wrap angle (deg) to the interval [-180.0, 180.0)
+inline double NormalizeAngleDeg( double angle )
+{
+  return NormalizeAngle(
+    angle,
+    -DEEGREE_HALF_ARC,
+    DEGREE_FULL_ARC,
+    DEGREE_HALF_ARC);
+}
+
+
+// wrap angle (rad) to the interval [0, 2PI)
+inline double UnsignedAngleRad( double angle )
+{
+  return NormalizeAngle(
+    angle,
+    0,
+    RADIAN_FULL_ARC,
+    RADIAN_HALF_ARC);
+}
+
+
+// wrap angle (rad) to the interval [-PI, PI)
+inline double NormalizeAngleRad( double angle )
+{
+  return NormalizeAngle(
+    angle,
+    -RADIAN_HALF_ARC,
+    RADIAN_FULL_ARC,
+    RADIAN_HALF_ARC);
+}
